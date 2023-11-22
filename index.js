db702c1ede6053f4cc3c9550d61e04ad7c0d396e
Andrew Di Jorio <acdijorio@gmail.com>
2023-11-22 06:56:12 -0600

    code complete

---
+++ index.js
@@ -0,0 +1,8 @@
+function divide (num1, num2) {
+    return ( num1 / num2)
+
+};
+
+const square = parameter => parameter * parameter;
+const add = (parameter, parameter2) => parameter + parameter2;
+
